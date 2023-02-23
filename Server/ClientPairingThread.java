207a9db35a7d243befde6188dc54aec636127d29
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:26:45 +0100

    Creazione classe

--- Server/ClientPairingThread.java
+++ Server/ClientPairingThread.java
@@ -1,1 +1,14 @@
+package Server;
 
+import Messages.Message;
+import chess_game.Pieces.Team;
+
+public class ClientPairingThread extends Thread {
+
+  SClient client;
+  
+  public ClientPairingThread(SClient client) {
+        this.client = client;
+  }
+  
+}
