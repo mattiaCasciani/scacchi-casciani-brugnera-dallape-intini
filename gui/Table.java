f6f97d7ba89ec083f852e9691a69495724aad048
Intinirocco <61295174+Intinirocco@users.noreply.github.com>
2023-02-23 18:16:59 +0100

    cerato il costruttore e gli attributi

--- gui/Table.java
+++ gui/Table.java
@@ -1,1 +1,42 @@
+package chess_game.gui;
 
+import ClientSide.Client;
+import Messages.Message;
+import chess_game.Boards.Board;
+
+import chess_game.Pieces.Team;
+import chess_game.Resources.GUI_Configurations;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+
+public class Table {
+
+    private JFrame gameFrame;
+    private BoardPanel boardPanel;
+    private Board chessBoard;
+    private MainMenu mainMenu;
+    private InGameBottomMenu bottomGameMenu;
+    private Client client;
+
+    public Table() {
+        this.gameFrame = new JFrame("Chess");
+        this.gameFrame.setLayout(new BorderLayout());
+        this.gameFrame.setSize(GUI_Configurations.OUTER_FRAME_DIMENSION);
+        this.mainMenu = new MainMenu();
+        this.client = new Client(this);
+        this.client.Connect("127.0.0.1", 4000);
+        if (this.client.socket == null) {
+            JOptionPane.showMessageDialog(null, "Impossibile collegarsi al server");
+            System.exit(0);
+        }
+        createMainMenu();
+        this.gameFrame.setVisible(true);
+
+    }
+}
