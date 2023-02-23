d19d7c2c1ef7b3520f386ff2b4be98d66eae35d9
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:36:42 +0100

    Creazione classe + cstr

--- Server/SClient.java
+++ Server/SClient.java
@@ -1,1 +1,24 @@
+package Server;
 
+import Messages.Message;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SClient {
+
+    public Socket socket;
+    public ObjectInputStream cInput;
+    public ObjectOutputStream cOutput;
+    public ClientListenThread clientListenThread;
+    public SClient pair;
+    public boolean isPaired;
+    public boolean isWantToPair = false;
+    public ClientPairingThread pairingThread;
+  
+  public SClient(Socket socket) {}
+
+}
