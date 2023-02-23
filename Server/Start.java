2955c79a73dc4b3def6622ad1d8e822f65944767
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:45:55 +0100

    fine main

--- Server/Start.java
+++ Server/Start.java
@@ -3,4 +3,17 @@ package Server;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Start {}
+public class Start {
+  Server server = new Server(4000);
+        server.ListenClientConnectionRequests();
+
+        while (!server.socket.isClosed()) {
+
+            try {
+                System.out.println("Sto aspettando qui."+Server.clients.size()+" Giocatori Connessi.");
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+}
