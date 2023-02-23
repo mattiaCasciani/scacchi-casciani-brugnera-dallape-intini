b755705ccff2ea34bbf5721556a291872712653e
Intinirocco <61295174+Intinirocco@users.noreply.github.com>
2023-02-23 17:37:34 +0100

    inseriti gli attributi e il costruttore

--- Boards/Tile.java
+++ Boards/Tile.java
@@ -1,1 +1,15 @@
+package chess_game.Boards;
 
+import chess_game.Pieces.Coordinate;
+import chess_game.Pieces.Piece;
+
+
+public class Tile implements java.io.Serializable{
+    private Piece piece;
+    private Coordinate coordinate;
+
+    public Tile(Coordinate coordinate, Piece piece) {
+        this.setPiece(piece);
+        this.setCoordinate(coordinate);
+    }
+}
