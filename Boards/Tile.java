afdcf3bbe5746a1b2633ca298e0ffe8038906949
Intinirocco <61295174+Intinirocco@users.noreply.github.com>
2023-02-23 17:39:16 +0100

    creati i metodi get e set
    creati i metodi getPiece, setPiece, getCoordinate

--- Boards/Tile.java
+++ Boards/Tile.java
@@ -12,4 +12,11 @@ public class Tile implements java.io.Serializable{
         this.setPiece(piece);
         this.setCoordinate(coordinate);
     }
+    
+    public Piece getPiece() {return this.piece;}
+    
+    public void setPiece(Piece p){this.piece = p;}
+    
+    public Coordinate getCoordinate() {return this.coordinate;}
+    
 }
