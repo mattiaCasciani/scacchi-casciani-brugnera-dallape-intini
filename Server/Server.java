b943159a0e79bfdba40319513e4f9e28807c4706
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:40:58 +0100

    Creazione costruttore

--- Server/Server.java
+++ Server/Server.java
@@ -1,1 +1,25 @@
+package Server;
+
+import Messages.Message;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server {
+
+    public ServerSocket socket;
+    public int port;
+    public ListenConnectionRequestThread listenConnectionRequestThread;
+    public ClientRemovingControlThread removingControlThread;
+    public static ArrayList<SClient> clients;
+  
+    public static Semaphore pairingLockForTwoPair = new Semaphore(1, true);
+  
+    public Server(int port) {
+        
+    }
+}
 
