0de89b83a324995bf68e16f5ba6a0afe03b2da86
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:32:57 +0100

    Creazione costruttore e run

--- Server/ClientRemovingControlThread.java
+++ Server/ClientRemovingControlThread.java
@@ -3,4 +3,22 @@ package Server;
 public class ClientRemovingControlThread extends Thread{
   private Server server;
   
+  public ClientRemovingControlThread(Server server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        
+        while(!this.server.socket.isClosed())
+        {
+            for(SClient client : Server.clients)
+            {
+                if(client.socket.isClosed())
+                {
+                    Server.clients.remove(client);
+                }
+            }
+        }
+    }
 }
