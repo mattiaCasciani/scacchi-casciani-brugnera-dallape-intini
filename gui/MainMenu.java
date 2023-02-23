17f6b75c5fac596d9b8ca5ec4334b6ea767f7bb8
Intinirocco <61295174+Intinirocco@users.noreply.github.com>
2023-02-23 18:05:33 +0100

    creato il costruttore e gli attributi

--- gui/MainMenu.java
+++ gui/MainMenu.java
@@ -1,1 +1,16 @@
+package chess_game.gui;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
+public class MainMenu extends javax.swing.JPanel {
+    private javax.swing.JButton exitBTN;
+    private javax.swing.JLabel infoLBL;
+    private javax.swing.JButton playBTN;
+  
+    public MainMenu() {
+        initComponents();
+
+    }
+}
