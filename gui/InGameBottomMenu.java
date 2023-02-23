b2ec0405bfd52e98f75603200197709b8f486a1a
Intinirocco <61295174+Intinirocco@users.noreply.github.com>
2023-02-23 18:00:27 +0100

    creato il costruttore e attributi

--- gui/InGameBottomMenu.java
+++ gui/InGameBottomMenu.java
@@ -1,1 +1,24 @@
+package chess_game.gui;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+
+public class InGameBottomMenu extends javax.swing.JPanel {
+
+    public DefaultListModel killedPiecesListModel;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JList<String> killedPiecesLIST;
+    private javax.swing.JLabel playersColorLBL;
+    private javax.swing.JLabel turnLBL;
+  
+  
+    public InGameBottomMenu() {
+        initComponents();
+        killedPiecesListModel = new DefaultListModel();
+        killedPiecesLIST.setModel(killedPiecesListModel);
+    }
+}
