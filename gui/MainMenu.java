1974bdb6468e48334bdcd1136b86e631d1cbece8
Intinirocco <61295174+Intinirocco@users.noreply.github.com>
2023-02-23 18:06:48 +0100

    creati i metodi get
    i metodi playBTNActionPerformed e exitBTNActionPerformed sono stati creati in automatico

--- gui/MainMenu.java
+++ gui/MainMenu.java
@@ -11,6 +11,26 @@ public class MainMenu extends javax.swing.JPanel {
   
     public MainMenu() {
         initComponents();
-
     }
+    
+    public JButton getExitBTN() {
+        return exitBTN;
+    }
+
+   
+    public JLabel getInfoLBL() {
+        return infoLBL;
+    }
+
+    public JButton getPlayBTN() {
+        return playBTN;
+    }
+    
+    private void playBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_playBTNActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_playBTNActionPerformed
+
+    private void exitBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitBTNActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitBTNActionPerformed
 }
