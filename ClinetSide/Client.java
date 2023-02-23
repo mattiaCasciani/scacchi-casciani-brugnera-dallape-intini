60b442148b2463b8f0c78e52a5ea6e29618c62a3
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:05:14 +0100

    Creazione classe

--- ClinetSide/Client.java
+++ ClinetSide/Client.java
@@ -1,1 +1,24 @@
+package ClientSide;
 
+import Messages.Message;
+import chess_game.Pieces.Team;
+import chess_game.gui.MainMenu;
+import chess_game.gui.Table;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client { 
+    public Socket socket;
+    public ObjectInputStream sInput;
+    public ObjectOutputStream sOutput;
+    private Team team = Team.NOCOLOR;
+    public boolean isPaired = false;
+    public String serverIP;
+    public int serverPort;
+    public ClientListenThread listenThread;
+    public Table game;
+}
