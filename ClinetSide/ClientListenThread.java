f648a39f1f6d9336596f24640df6c8a681537ced
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:13:04 +0100

    Iniziamo la classe

--- ClinetSide/ClientListenThread.java
+++ ClinetSide/ClientListenThread.java
@@ -1,1 +1,19 @@
+package ClientSide;
 
+import Messages.Message;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ClientSide.*;
+import Messages.MovementMessage;
+import chess_game.Boards.Board;
+import chess_game.Move.Move;
+import chess_game.Pieces.PieceTypes;
+import chess_game.Pieces.Team;
+import chess_game.Player.Player;
+import java.awt.Color;
+import javax.swing.JOptionPane;
+
+public class ClientListenThread extends Thread {
+    Client client;
+}
