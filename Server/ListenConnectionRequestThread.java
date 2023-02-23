58ce9111f8512021d4c3ea78fd585c98696cedde
Chanky <50941944+LinelessChanky@users.noreply.github.com>
2023-02-23 17:33:55 +0100

    Creazione classe

--- Server/ListenConnectionRequestThread.java
+++ Server/ListenConnectionRequestThread.java
@@ -1,1 +1,8 @@
+package Server;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ListenConnectionRequestThread extends Thread {}
